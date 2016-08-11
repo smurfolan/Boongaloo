1edfdcd4b31bfa73342b9dee3c60fb55e5ebc851
Kristian Azmanov <Kristian_Azmanov@epam.com>
2016-08-11 16:04:43 +0300

    Created groups controller and added endpoint markers.

---
+++ Boongaloo/Boongaloo.API/Controllers/GroupsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Boongaloo.API.Controllers
+{
+    public class GroupsController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetGroupsForCoordinates(double latitude, double longitude)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewGroup(double lattitude, double longitude)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetGroupById(int groupId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
