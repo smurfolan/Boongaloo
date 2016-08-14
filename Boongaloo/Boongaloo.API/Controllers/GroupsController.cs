b33449ac1496a8fcd7c40f730a8f189dda4b9034
kristian.azmanov <kristian.azmanov@afusion.com>
2016-08-14 18:01:14 +0300

    Best practices API improvements.

--- Boongaloo/Boongaloo.API/Controllers/GroupsController.cs
+++ Boongaloo/Boongaloo.API/Controllers/GroupsController.cs
@@ -7,23 +7,45 @@ using System.Web.Http;
 
 namespace Boongaloo.API.Controllers
 {
+    //[Authorize]
+    [RoutePrefix("api/v1/groups")]
     public class GroupsController : ApiController
     {
-        [HttpGet]
-        public IHttpActionResult GetGroupsForCoordinates(double latitude, double longitude)
+        public GroupsController(/*Comma separated arguments of type interface*/)
         {
+            // Handle assignment by DI
+        }
+
+        // GET /api/v1/groups/34.234456/42.234/
+        [HttpGet]
+        [Route("{lat:double}/{lon:double}")]
+        public IHttpActionResult Get(double lat, double lon)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             throw new NotImplementedException();
         }
 
+        // POST /api/v1/groups/34.234456/42.234/
         [HttpPost]
-        public IHttpActionResult CreateNewGroup(double lattitude, double longitude)
+        [Route("{lat:double}/{lon:double}")]
+        public IHttpActionResult Post(double lat, double lon)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             throw new NotImplementedException();
         }
 
+        // GET /api/v1/groups/342342
         [HttpGet]
-        public IHttpActionResult GetGroupById(int groupId)
+        [Route("{id:int}")]
+        public IHttpActionResult Get(int id)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             throw new NotImplementedException();
         }
     }
