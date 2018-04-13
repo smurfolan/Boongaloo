cd5d643f188bb43d038f9cef9015681b7e49358b
Kristian Azmanov <kristian.azmanov@gmail.com>
2018-04-13 12:15:22 +0300

    Efforts to update external login window for facebook

---
+++ Boongaloo/BoongalooCompany.IdentityServer/Helpers/FacebookAuthenticationProviderWrapper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin.Security.Facebook;
+
+namespace BoongalooCompany.IdentityServer.Helpers
+{
+    public class FacebookAuthenticationProviderWrapper : FacebookAuthenticationProvider
+    {
+        public override void ApplyRedirect(FacebookApplyRedirectContext context)
+        {
+            context.Response.Redirect(context.RedirectUri + "&display=popup");
+        }
+    }
+}
